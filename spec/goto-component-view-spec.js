c7667e0c3cd1dc81fc8dd8f437f4c3e666d77472
Leyuan <leyuan.ly@gmail.com>
2016-10-06 11:45:14 -0600

    first commit

---
+++ spec/goto-component-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import GotoComponentView from '../lib/goto-component-view';
+
+describe('GotoComponentView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
